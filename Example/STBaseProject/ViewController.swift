e6147ce685eb9256b75a2a836668b65e8b1de7f0
songs <songshoubing7664@163.com>
2019-05-16 11:41:33 +0800

    Initial commit

---
+++ Example/STBaseProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  STBaseProject
+//
+//  Created by songMW on 05/16/2019.
+//  Copyright (c) 2019 songMW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
