6e1e7fef4227e466973b5df5dea11b0bf2540367
songs <songshoubing7664@163.com>
2020-01-15 14:32:18 +0800

    Update STBaseView.swift

--- STBaseProject/Classes/STBaseModule/STBaseView/STBaseView.swift
+++ STBaseProject/Classes/STBaseModule/STBaseView/STBaseView.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
+public protocol STBaseViewDelegate: NSObjectProtocol {
+    func st_numberOfSections() -> Int
+    func st_numberOfRowsIn(section: Int) -> Int
+    func st_didSelectRowAt(section: Int, row: Int)
+}
+
 open class STBaseView: UIView {
 
+    open weak var delegate: STBaseViewDelegate?
+    
+    public var tableView: UITableView?
     public var baseContentView: UIView?
     public var baseScrollView: UIScrollView?
 
@@ -28,6 +37,11 @@ open class STBaseView: UIView {
         self.baseScrollView?.addSubview(self.baseContentView ?? UIView())
     }
     
+    public func st_baseViewAddTableView(frame: CGRect, style: UITableView.Style) -> Void {
+        self.tableView = self.createTableView(frame: frame, style: style)
+        self.addSubview(self.tableView ?? UITableView())
+    }
+    
     func createScrollView() -> UIScrollView {
         let scrollView = UIScrollView()
         scrollView.isPagingEnabled = true
@@ -39,5 +53,33 @@ open class STBaseView: UIView {
     func createContentView() -> UIView {
         let view = UIView()
         return view
+    }
+    
+    func createTableView(frame: CGRect, style: UITableView.Style) -> UITableView {
+        let tableView = UITableView.init(frame: frame, style: style)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        return tableView
+    }
+}
+
+extension STBaseView: UITableViewDelegate, UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let newDelegate = self.delegate {
+            return newDelegate.st_numberOfRowsIn(section: section)
+        }
+        return 0
+    }
+    
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        if let newDelegate = self.delegate {
+            return newDelegate.st_numberOfSections()
+        }
+        return 0
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
