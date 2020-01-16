b1f9de0bf82217a54da0a75969a2abb9a9da5df0
songs <songshoubing7664@163.com>
2020-01-16 17:14:24 +0800

    Update STBaseView.swift

--- STBaseProject/Classes/STBaseModule/STBaseView/STBaseView.swift
+++ STBaseProject/Classes/STBaseModule/STBaseView/STBaseView.swift
@@ -61,10 +61,6 @@ extension STBaseView: UITableViewDelegate, UITableViewDataSource {
         return 0
     }
     
-    open func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
     open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
