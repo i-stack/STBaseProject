6e7c97ae1146dba010555d4a3748733d007918a6
songs <songshoubing7664@163.com>
2020-05-09 14:39:22 +0800

    Update STBaseModel.swift

--- STBaseProject/Classes/STBaseModule/STBaseModel/STBaseModel.swift
+++ STBaseProject/Classes/STBaseModule/STBaseModel/STBaseModel.swift
@@ -26,12 +26,51 @@ open class STBaseModel: NSObject {
     open override func setValue(_ value: Any?, forUndefinedKey key: String) {
         print("⚠️ ⚠️ Key = \(key) isUndefinedKey ⚠️ ⚠️")
     }
-    
-    /// 字典转模型
-    public func st_toModel<T>(_ type: T.Type, value: Any) -> T? where T : Decodable {
+}
+
+public extension STBaseModel {
+    /// json转model
+    func st_jsonToModel<T>(_ type: T.Type, value: Any) -> T? where T : Decodable {
         guard let data = try? JSONSerialization.data(withJSONObject: value) else { return nil }
         let decoder = JSONDecoder()
-        decoder.nonConformingFloatDecodingStrategy = .convertFromString(positiveInfinity: "+Infinity", negativeInfinity: "-Infinity", nan: "NaN")
         return try? decoder.decode(type, from: data)
+    }
+    
+    static func jsonToModel<T>(_ type: T.Type, value: Any) -> T? where T : Decodable {
+        return STBaseModel().st_jsonToModel(type, value: value)
+    }
+    
+    /// model转json
+    func st_modelToJson<T>(value: T) -> String where T : Encodable {
+        if let jsonData = try? JSONEncoder().encode(value) {
+            if let jsonString = String.init(data: jsonData, encoding: String.Encoding.utf8) {
+                return jsonString
+            }
+        }
+        return ""
+    }
+    
+    static func modelToJson<T>(value: T) -> String where T : Encodable {
+        return STBaseModel().st_modelToJson(value: value)
+    }
+    
+    /// array转json
+    func st_arrayToJson<T>(value: T) -> String where T : Encodable {
+        var jsonStr = ""
+        if JSONSerialization.isValidJSONObject(value) {
+            do {
+                let data = try JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+                if let str = String.init(data: data, encoding: .utf8) {
+                    jsonStr = str
+                }
+            } catch {
+                
+            }
+        }
+        return jsonStr
+    }
+    
+    static func arrayToJson<T>(value: T) -> String where T : Encodable {
+        return STBaseModel().st_arrayToJson(value: value)
     }
 }
