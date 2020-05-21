e6c7ed011a4e9c3907df05f04a3f23bd1b8cd502
songs <songshoubing7664@163.com>
2020-05-21 16:03:43 +0800

    Update STImage.swift

--- STBaseProject/Classes/STBaseModule/STCommon/STImage.swift
+++ STBaseProject/Classes/STBaseModule/STCommon/STImage.swift
@@ -115,4 +115,38 @@ public extension UIImage {
         }
         return .STImageFormatUndefined
     }
+    
+    /// 获取图片某一点的颜色
+    func st_getPointColor(point: CGPoint) -> UIColor {
+        guard CGRect(origin: CGPoint(x: 0, y: 0), size: self.size).contains(point) else {
+            return UIColor.clear
+        }
+
+        let pointX = trunc(point.x);
+        let pointY = trunc(point.y);
+
+        let width = self.size.width;
+        let height = self.size.height;
+        let colorSpace = CGColorSpaceCreateDeviceRGB();
+        var pixelData: [UInt8] = [0, 0, 0, 0]
+
+        pixelData.withUnsafeMutableBytes { pointer in
+            if let context = CGContext(data: pointer.baseAddress, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace, bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue), let cgImage = self.cgImage {
+                context.setBlendMode(.copy)
+                context.translateBy(x: -pointX, y: pointY - height)
+                context.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+            }
+        }
+
+        let red = CGFloat(pixelData[0]) / CGFloat(255.0)
+        let green = CGFloat(pixelData[1]) / CGFloat(255.0)
+        let blue = CGFloat(pixelData[2]) / CGFloat(255.0)
+        let alpha = CGFloat(pixelData[3]) / CGFloat(255.0)
+
+        if #available(iOS 10.0, *) {
+            return UIColor(displayP3Red: red, green: green, blue: blue, alpha: alpha)
+        } else {
+            return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        }
+    }
 }
