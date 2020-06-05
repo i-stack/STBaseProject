ed3260ffe76fb257eea546838476c0359d3faa66
songs <songshoubing7664@163.com>
2020-06-05 15:54:53 +0800

    Update STDateManager.swift

--- STBaseProject/Classes/STBaseModule/STCommon/STDateManager.swift
+++ STBaseProject/Classes/STBaseModule/STCommon/STDateManager.swift
@@ -63,6 +63,10 @@ public extension String {
         if let date = dateFormatter.date(from: self) {
             interval = date.timeIntervalSince1970
         }
+        let intervalStr = NSDecimalNumber.init(value: interval).stringValue
+        if intervalStr.count < 13 {
+            interval = interval * 1000
+        }
         return interval
     }
     
