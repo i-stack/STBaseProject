8e5935614fd9bb11e2766c9c1e7a02b3554aed98
songs <songshoubing7664@163.com>
2020-07-01 10:30:59 +0800

    Update STBtn.swift

--- STBaseProject/Classes/STBaseModule/STCommon/STBtn.swift
+++ STBaseProject/Classes/STBaseModule/STCommon/STBtn.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 public enum STBtnEdgeInsetsStyle {
-    case STBtnEdgeInsetsStyle_Top    // image在上，label在下
-    case STBtnEdgeInsetsStyle_Left   // image在左，label在右
-    case STBtnEdgeInsetsStyle_Right  // image在右，label在左
-    case STBtnEdgeInsetsStyle_Bottom // image在下，label在上
+    case top    // image在上，label在下
+    case left   // image在左，label在右
+    case right  // image在右，label在左
+    case bottom // image在下，label在上
+    case reset  // 复原
 }
 
 open class STBtn: UIButton {
@@ -71,21 +72,23 @@ open class STBtn: UIButton {
         var labelEdgeInsets = UIEdgeInsets.zero
         
         switch (style) {
-        case .STBtnEdgeInsetsStyle_Top:
+        case .top:
             imageEdgeInsets = UIEdgeInsets.init(top: -labelHeight - imageTitleSpace / 2.0, left: 0, bottom: 0, right: -labelWidth)
             labelEdgeInsets = UIEdgeInsets.init(top: 0, left: -imageWith, bottom: -imageHeight - imageTitleSpace / 2.0, right: 0)
             break
-        case .STBtnEdgeInsetsStyle_Left:
+        case .left:
             imageEdgeInsets = UIEdgeInsets.init(top: 0, left: -imageTitleSpace / 2.0, bottom: 0, right: imageTitleSpace / 2.0)
             labelEdgeInsets = UIEdgeInsets.init(top: 0, left: imageTitleSpace / 2.0, bottom: 0, right: -imageTitleSpace / 2.0)
             break
-        case .STBtnEdgeInsetsStyle_Bottom:
+        case .bottom:
             imageEdgeInsets = UIEdgeInsets.init(top: 0, left: 0, bottom: -labelHeight - imageTitleSpace / 2.0, right: -labelWidth)
             labelEdgeInsets = UIEdgeInsets.init(top: -imageHeight - imageTitleSpace / 2.0, left: -imageWith, bottom: 0, right: 0)
             break
-        case .STBtnEdgeInsetsStyle_Right:
+        case .right:
             imageEdgeInsets = UIEdgeInsets.init(top: 0, left: labelWidth + imageTitleSpace / 2.0, bottom: 0, right: -labelWidth - imageTitleSpace / 2.0)
             labelEdgeInsets = UIEdgeInsets.init(top: 0, left: -imageWith - imageTitleSpace / 2.0, bottom: 0, right: imageWith + imageTitleSpace / 2.0)
+            break
+        case .reset:
             break
         }
         
@@ -98,8 +101,8 @@ open class STBtn: UIButton {
     }
     
     public func st_roundedButton(cornerRadius: CGFloat, borderWidth: CGFloat, borderWidthColor: UIColor) -> Void {
-        self.cornerRadius = cornerRadius
         self.borderWidth = borderWidth
+        self.cornerRadius = cornerRadius
         self.borderColor = borderWidthColor
     }
 }
