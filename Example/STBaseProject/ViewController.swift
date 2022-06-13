e67a64ceba5a1f91389ec6643686e63c5a986546
stack <songshoubing7664@163.com>
2022-06-13 17:36:17 +0800

    Update ViewController.swift

--- Example/STBaseProject/ViewController.swift
+++ Example/STBaseProject/ViewController.swift
@@ -16,11 +16,32 @@ class ViewController: STBaseViewController {
         super.viewDidLoad()
         self.st_showNavBtnType(type: .onlyShowTitle)
         self.titleLabel.text = "ViewController"
+        randomString()
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
         let nextVC = STTestViewController.init(nibName: "STTestViewController", bundle: nil)
         self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    func randomString() {
+        var dict: Dictionary<String, String> = Dictionary<String, String>()
+        for i in 0..<1000 {
+            dict["key\(i)"] = "\(i)"
+        }
+        
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: dict, options: .prettyPrinted)
+            let outputPath = "\(STFileManager.getLibraryCachePath())/jsonData"
+            let pathIsExist = STFileManager.fileExistAt(path: outputPath)
+            let path = STFileManager.create(filePath: outputPath, fileName: "json.json")
+            print(outputPath)
+            try jsonData.write(to: URL.init(fileURLWithPath: path), options: .atomic)
+        } catch {
+            
+        }
+        
+        
     }
 }
