ebcf2b758ba9c18bf891380f2264f46086b3b0ff
i-stack <songshoubing7664@163.com>
2025-01-03 14:35:11 +0800

    重新调整podspec文件结构

--- STBaseProject.podspec
+++ STBaseProject.podspec
@@ -24,41 +24,50 @@ Pod::Spec.new do |s|
         'STBaseProject/Classes/STBaseModule/STBaseViewController/*.swift',
         'STBaseProject/Classes/STBaseModule/STBaseViewModel/*.swift',
         'STBaseProject/Classes/STBaseModule/STBaseModel/*.swift',
-        'STBaseProject/Classes/STBaseModule/STBaseView/*.swift'
+        'STBaseProject/Classes/STBaseModule/STBaseView/*.swift',
+        'STBaseProject/Classes/STBaseModule/*.swift',
+        'STBaseProject/Classes/STBaseModule/STCustomTools/STHUD/*.swift',
+        'STBaseProject/Classes/STBaseModule/STCustomTools/STHTTP/*.swift',
+        'STBaseProject/Classes/STBaseModule/STCustomTools/STOther/*.swift',
+        'STBaseProject/Classes/STBaseModule/STCustomTools/STString/*.swift',
+        'STBaseProject/Classes/STBaseModule/STCustomTools/STThread/*.swift',
+        'STBaseProject/Classes/STBaseModule/STCustomTools/STManager/*.swift',
+        'STBaseProject/Classes/STBaseModule/STCustomTools/STUIControl/*.swift',
+        'STBaseProject/Classes/STBaseModule/STCustomTools/STDeviceInfo/*.swift'
     ]
-    ss.dependency 'STBaseProject/STCustomTools'
+#    ss.dependency 'STBaseProject/STCustomTools'
   end
   
-  s.subspec 'STBaseConfig' do |ss|
-    ss.source_files = ['STBaseProject/Classes/STBaseConfig/*.swift']
-  end
-  
-  s.subspec 'STCustomTools' do |ss|
-    ss.source_files = [
-      'STBaseProject/Classes/STCustomTools/*.swift',
-      'STBaseProject/Classes/STCustomTools/STHUD/*.swift',
-      'STBaseProject/Classes/STCustomTools/STHTTP/*.swift',
-      'STBaseProject/Classes/STCustomTools/STOther/*.swift',
-      'STBaseProject/Classes/STCustomTools/STString/*.swift',
-      'STBaseProject/Classes/STCustomTools/STThread/*.swift',
-      'STBaseProject/Classes/STCustomTools/STManager/*.swift',
-      'STBaseProject/Classes/STCustomTools/STUIControl/*.swift',
-      'STBaseProject/Classes/STCustomTools/STDeviceInfo/*.swift'
-    ]
-    ss.dependency 'STBaseProject/STBaseConfig'
-  end
+#  s.subspec 'STBaseConfig' do |ss|
+#    ss.source_files = ['STBaseProject/Classes/STBaseConfig/*.swift']
+#  end
+#  
+#  s.subspec 'STCustomTools' do |ss|
+#    ss.source_files = [
+#      'STBaseProject/Classes/STCustomTools/*.swift',
+#      'STBaseProject/Classes/STCustomTools/STHUD/*.swift',
+#      'STBaseProject/Classes/STCustomTools/STHTTP/*.swift',
+#      'STBaseProject/Classes/STCustomTools/STOther/*.swift',
+#      'STBaseProject/Classes/STCustomTools/STString/*.swift',
+#      'STBaseProject/Classes/STCustomTools/STThread/*.swift',
+#      'STBaseProject/Classes/STCustomTools/STManager/*.swift',
+#      'STBaseProject/Classes/STCustomTools/STUIControl/*.swift',
+#      'STBaseProject/Classes/STCustomTools/STDeviceInfo/*.swift'
+#    ]
+#    ss.dependency 'STBaseProject/STBaseConfig'
+#  end
   
 #  s.subspec 'STOptionalTools' do |ss|
 #    ss.source_files = ['STBaseProject/Classes/STOptionalTools/*.swift']
 #  end
     
-  s.subspec 'STScanner' do |ss|
-    ss.source_files = ['STBaseProject/Classes/STOptionalTools/STScanner/*.swift']
-  end
-
-  s.subspec 'STScreenshot' do |ss|
-    ss.source_files = ['STBaseProject/Classes/STOptionalTools/STScreenshot/*.swift']
-  end
+#  s.subspec 'STScanner' do |ss|
+#    ss.source_files = ['STBaseProject/Classes/STOptionalTools/STScanner/*.swift']
+#  end
+#
+#  s.subspec 'STScreenshot' do |ss|
+#    ss.source_files = ['STBaseProject/Classes/STOptionalTools/STScreenshot/*.swift']
+#  end
 
 #  s.subspec 'STHUD' do |ss|
 #    ss.source_files = ['STBaseProject/Classes/STOptionalTools/STHUD/*.swift']
--- STBaseProject/Classes/STBaseModule/STBaseView/STBaseView.swift
+++ STBaseProject/Classes/STBaseModule/STBaseView/STBaseView.swift
@@ -31,17 +31,17 @@ open class STBaseView: UIView {
         self.addSubview(self.baseScrollView )
         self.baseScrollView.addSubview(self.baseContentView)
         self.addConstraints([
-                            NSLayoutConstraint.init(item: self.baseScrollView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0),
-                            NSLayoutConstraint.init(item: self.baseScrollView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 0),
-                            NSLayoutConstraint.init(item: self.baseScrollView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0),
-                            NSLayoutConstraint.init(item: self.baseScrollView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: 0)
+            NSLayoutConstraint.init(item: self.baseScrollView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint.init(item: self.baseScrollView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 0),
+            NSLayoutConstraint.init(item: self.baseScrollView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0),
+            NSLayoutConstraint.init(item: self.baseScrollView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: 0)
         ])
         self.baseScrollView.addConstraints([
-                                            NSLayoutConstraint.init(item: self.baseContentView, attribute: .width, relatedBy: .equal, toItem: self.baseScrollView, attribute: .width, multiplier: 1, constant: 0),
-                                            NSLayoutConstraint.init(item: self.baseContentView, attribute: .top, relatedBy: .equal, toItem: self.baseScrollView, attribute: .top, multiplier: 1, constant: 0),
-                                            NSLayoutConstraint.init(item: self.baseContentView, attribute: .leading, relatedBy: .equal, toItem: self.baseScrollView, attribute: .leading, multiplier: 1, constant: 0),
-                                            NSLayoutConstraint.init(item: self.baseContentView, attribute: .bottom, relatedBy: .equal, toItem: self.baseScrollView, attribute: .bottom, multiplier: 1, constant: 0),
-                                            NSLayoutConstraint.init(item: self.baseContentView, attribute: .trailing, relatedBy: .equal, toItem: self.baseScrollView, attribute: .trailing, multiplier: 1, constant: 0)
+            NSLayoutConstraint.init(item: self.baseContentView, attribute: .width, relatedBy: .equal, toItem: self.baseScrollView, attribute: .width, multiplier: 1, constant: 0),
+            NSLayoutConstraint.init(item: self.baseContentView, attribute: .top, relatedBy: .equal, toItem: self.baseScrollView, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint.init(item: self.baseContentView, attribute: .leading, relatedBy: .equal, toItem: self.baseScrollView, attribute: .leading, multiplier: 1, constant: 0),
+            NSLayoutConstraint.init(item: self.baseContentView, attribute: .bottom, relatedBy: .equal, toItem: self.baseScrollView, attribute: .bottom, multiplier: 1, constant: 0),
+            NSLayoutConstraint.init(item: self.baseContentView, attribute: .trailing, relatedBy: .equal, toItem: self.baseScrollView, attribute: .trailing, multiplier: 1, constant: 0)
         ])
     }
     
