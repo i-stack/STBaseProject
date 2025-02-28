0774e08cac185f79bad8db9f86bf260583c7ae2e
i-stack <songshoubing7664@163.com>
2025-02-28 17:57:23 +0800

    add wkwebviewcontroller

--- STBaseProject/Classes/STBaseModule/STBaseViewController/STBaseViewController.swift
+++ STBaseProject/Classes/STBaseModule/STBaseViewController/STBaseViewController.swift
@@ -41,7 +41,7 @@ open class STBaseViewController: UIViewController {
     open var navBgViewAttributeHeight: NSLayoutConstraint!
 
     deinit {
-        
+        STLog("🌈 -> \(self) 🌈 ----> 🌈 dealloc")
     }
 
     override open func viewDidLoad() {
@@ -204,5 +204,63 @@ extension STBaseViewController: UIGestureRecognizerDelegate {
             return true
         }
         return false
+    }
+}
+
+public extension STBaseViewController {
+    func st_isDark() -> Bool {
+        if #available(iOS 12.0, *) {
+            if self.traitCollection.userInterfaceStyle == .dark {
+                return true
+            }
+        }
+        return false
+    }
+    
+    // effect as present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)? = nil)
+    func st_setPushAnimatedPresentWithTransition(customSelf: UINavigationController) {
+        let animation = CATransition.init()
+        animation.duration = 0.3
+        animation.type = CATransitionType.moveIn
+        animation.subtype = CATransitionSubtype.fromTop
+        customSelf.view.layer.add(animation, forKey: nil)
+    }
+    
+    // effect as dismiss(animated: Bool, completion: (() -> Void)? = nil)
+    func st_setPushAnimatedDismissWithTransition(customSelf: UINavigationController) {
+        let animation = CATransition.init()
+        animation.duration = 0.3
+        animation.type = CATransitionType.moveIn
+        animation.subtype = CATransitionSubtype.fromBottom
+        customSelf.view.layer.add(animation, forKey: nil)
+    }
+    
+    func st_currentVC() -> UIViewController {
+        if let keyWindow = self.view.st_keyWindow() {
+            if let rootViewController = keyWindow.rootViewController {
+                let currentViewController = self.st_getCurrentViewControllerFrom(rootVC: rootViewController)
+                return currentViewController
+            }
+        }
+        return UIViewController()
+    }
+    
+    func st_getCurrentViewControllerFrom(rootVC: UIViewController) -> UIViewController {
+        var currentVC = rootVC
+        if let currentViewContoller = rootVC.presentedViewController {
+            currentVC = currentViewContoller
+        }
+        if rootVC.isKind(of: UINavigationController.self) {
+            let nav = rootVC as! UINavigationController
+            if let visibleViewController = nav.visibleViewController {
+                currentVC = self.st_getCurrentViewControllerFrom(rootVC: visibleViewController)
+            }
+        } else if rootVC.isKind(of: UITabBarController.self) {
+            let tabBar = rootVC as! UITabBarController
+            if let selectedViewController = tabBar.selectedViewController {
+                currentVC = self.st_getCurrentViewControllerFrom(rootVC: selectedViewController)
+            }
+        }
+        return currentVC
     }
 }
