9baee516e67c954458f9a603db443a99209197de
stack <songshoubing7664@163.com>
2025-09-08 17:25:54 +0800

    Update STHTTPSession.swift

--- STBaseProject/Classes/STKit/Network/STHTTPSession.swift
+++ STBaseProject/Classes/STKit/Network/STHTTPSession.swift
@@ -721,6 +721,39 @@ public struct STSSLPinningConfig: Codable {
         self.validateHost = validateHost
         self.allowInvalidCertificates = allowInvalidCertificates
     }
+    
+    // MARK: - Codable 实现
+    private enum CodingKeys: String, CodingKey {
+        case enabled
+        case certificates
+        case publicKeyHashes
+        case validateHost
+        case allowInvalidCertificates
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        enabled = try container.decode(Bool.self, forKey: .enabled)
+        validateHost = try container.decode(Bool.self, forKey: .validateHost)
+        allowInvalidCertificates = try container.decode(Bool.self, forKey: .allowInvalidCertificates)
+        publicKeyHashes = try container.decode([String].self, forKey: .publicKeyHashes)
+        
+        // 解码 Base64 编码的证书数据
+        let certificateStrings = try container.decode([String].self, forKey: .certificates)
+        certificates = certificateStrings.compactMap { Data(base64Encoded: $0) }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(enabled, forKey: .enabled)
+        try container.encode(validateHost, forKey: .validateHost)
+        try container.encode(allowInvalidCertificates, forKey: .allowInvalidCertificates)
+        try container.encode(publicKeyHashes, forKey: .publicKeyHashes)
+        
+        // 将证书数据编码为 Base64 字符串
+        let certificateStrings = certificates.map { $0.base64EncodedString() }
+        try container.encode(certificateStrings, forKey: .certificates)
+    }
 }
 
 // MARK: - 网络安全检测
