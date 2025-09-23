f718915b43d610c4d18c7950be0792cab7826798
stack <songshoubing7664@163.com>
2025-09-23 08:37:03 +0800

    pod install

--- Example/Pods/Target Support Files/STBaseProject/STBaseProject-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_STBaseProject : NSObject
-@end
-@implementation PodsDummy_STBaseProject
-@end
---
+++ Example/STBaseProject/View/STMyScrollableView.swift
@@ -0,0 +1,22 @@
+//
+//  STMyScrollableView.swift
+//  STBaseProject_Example
+//
+//  Created by song on 2025/9/23.
+//  Copyright © 2025 STBaseProject. All rights reserved.
+//
+
+import UIKit
+import STBaseProject
+
+class STMyScrollableView: STBaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
