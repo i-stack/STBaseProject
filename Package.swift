755565613d22eb9d4eade1bd4bddb5bab2a95926
stack <songshoubing7664@163.com>
2025-09-27 09:19:34 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,20 +10,8 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "STBase",
+            name: "STBaseProject",
             targets: ["STBase"]
-        ),
-        .library(
-            name: "STLocation",
-            targets: ["STLocation"]
-        ),
-        .library(
-            name: "STContacts",
-            targets: ["STContacts"]
-        ),
-        .library(
-            name: "STMedia",
-            targets: ["STMedia"]
         ),
     ],
     targets: [
@@ -31,21 +19,6 @@ let package = Package(
             name: "STBase",
             dependencies: [],
             path: "Sources/STBase"
-        ),
-        .target(
-            name: "STLocation",
-            dependencies: [],
-            path: "Sources/STLocation"
-        ),
-        .target(
-            name: "STContacts",
-            dependencies: [],
-            path: "Sources/STContacts"
-        ),
-        .target(
-            name: "STMedia",
-            dependencies: [],
-            path: "Sources/STMedia"
         ),
     ],
     swiftLanguageVersions: [.v5]
